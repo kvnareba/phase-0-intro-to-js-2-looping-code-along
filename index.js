cdd3429afbab117747da35172b31d97b09ee357b
kvn <kvnareba@gmail.com>
2024-04-04 12:09:58 +0300

    con

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(george, birthday) {
+    let cardsArray = {};
+    for (let 1=0; 1 < george,length; 1++){
+        cardsArray.push(`Thank you, ${george{1}},for the wonderful ${birthday} gift`);}
+        return cardsArray;
+    }
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number==;
+    }
+}
