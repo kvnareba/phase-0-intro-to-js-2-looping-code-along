377994d839b3eb615553207211c1cf455d8e5f0e
kvn <kvnareba@gmail.com>
2024-04-16 10:46:36 +0300

    com

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 function writeCards(george, birthday) {
-    let cardsArray = {};
-    for (let 1=0; 1 < george,length; 1++){
-        cardsArray.push(`Thank you, ${george{1}},for the wonderful ${birthday} gift`);}
-        return cardsArray;
+    let cardsArray = [];
+    for (let i = 0; i < george.length; i++) {
+        cardsArray.push(`Thank you, ${george[i]}, for the wonderful ${birthday} gift!`);
     }
+    return cardsArray;
+}
 
 function countDown(number) {
     while (number >= 0) {
         console.log(number);
-        number==;
+        number--;
     }
 }
+ 
